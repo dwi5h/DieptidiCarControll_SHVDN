56ca61ce18387e2b952498cf10174628f5dca1f9
dwi5h <dwish.rezpector@gmail.com>
2023-07-22 12:47:37 +0700

    update integrating with DieptidiBaseMenu

--- DieptidiCarControll_SHVDN/Main.cs
+++ DieptidiCarControll_SHVDN/Main.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using DieptidiUtility_SHVDN;
 using DieptidiCarControll_SHVDN.Controller;
+using DieptidiBaseMenu_SHVDN;
 using GTA;
 using GTA.Math;
 using GTA.UI;
@@ -13,42 +14,19 @@ using NativeUI;
 
 namespace DieptidiCarControll_SHVDN
 {
-    public class Main : Script
+    public class Main : DieptidiBaseMenu
     {
         bool textOn = false;
-        List<Vehicle> vehicles;
-        List<Blip> blips;
+        static List<Vehicle> vehicles;
+        static List<Blip> blips;
 
-        MenuPool menuPool;
-        public UIMenu UImenuCarControll;
-        UIMenuItem Lock, Unlock, Reload;
-
-        public Main()
+        public Main() : base()
         {
             try
             {
                 StorageController.CreateDirectory();
                 vehicles = VehicleLockSystemController.LoadAllVehicle().ToList();
                 blips = BlipController.RenderBlips(vehicles).ToList();
-
-                Lock = new UIMenuItem("Lock");
-                Unlock = new UIMenuItem("Unlock");
-                Reload = new UIMenuItem("Reload");
-
-                UImenuCarControll = new UIMenu("Car Controll", "");
-                UImenuCarControll.MouseControlsEnabled = true;
-                UImenuCarControll.AddItem(Lock);
-                UImenuCarControll.AddItem(Unlock);
-                UImenuCarControll.AddItem(Reload);
-
-                UImenuCarControll.OnItemSelect += UImenuCarControll_OnItemSelect;
-
-                menuPool = new MenuPool();
-                menuPool.Add(UImenuCarControll);
-
-                Tick += Main_Tick;
-                KeyUp += Main_KeyUp;
-                Aborted += Main_Aborted;
 
                 Notification.Show("Car Controll ~g~Reloaded");
             }
@@ -58,48 +36,40 @@ namespace DieptidiCarControll_SHVDN
             }
         }
 
-        private void Main_Aborted(object sender, EventArgs e)
+        public override Action<UIMenuItem, UIMenu> OnItemSelect => (_item, _menu) =>
         {
-            VehicleLockSystemController.Dispose(vehicles);
-            BlipController.DisposeBlips(blips);
-        }
-
-        private void UImenuCarControll_OnItemSelect(UIMenu sender, UIMenuItem selectedItem, int index)
-        {
-            switch (index)
+            switch (_item.Text)
             {
-                case 0:
+                case "Lock":
                     VehicleLockSystemController.Lock(vehicles, blips);
-                    UImenuCarControll.Visible = false;
+                    _menu.Visible = false;
                     break;
-                case 1:
+                case "Unlock":
                     VehicleLockSystemController.Unlock(vehicles, blips);
-                    UImenuCarControll.Visible = false;
+                    _menu.Visible = false;
                     break;
-                case 2:
+                case "Reload":
                     VehicleLockSystemController.Reload(vehicles, blips);
-                    UImenuCarControll.Visible = false;
+                    _menu.Visible = false;
                     break;
                 default:
                     break;
             }
-        }
+        };
 
-        private void Main_KeyUp(object sender, KeyEventArgs e)
+        public override Action Aborted => () =>
         {
-            if (e.KeyCode == Keys.F1)
-            {
-                UImenuCarControll.Visible = true;
-            }
-        }
+            VehicleLockSystemController.Dispose(vehicles);
+            BlipController.DisposeBlips(blips);
+        };
 
-        private void Main_Tick(object sender, EventArgs e)
+        public override Action Tick => null;
+
+        public override void BuildItem()
         {
-            menuPool.ProcessMenus();
-            //if (textOn)
-            //{
-            //    Helper.Draw3dText(vehicle.Position, vehicle.EngineHealth.ToString());
-            //}
+            MenuItems.Add(new UIMenuItem("Lock"));
+            MenuItems.Add(new UIMenuItem("Unlock"));
+            MenuItems.Add(new UIMenuItem("Reload"));
         }
     }
 }
